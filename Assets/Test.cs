8929d4d89b03b5629c9c29e10c4d4858da0e145f
KenichiHiroi <151005957+KenichiHiroi@users.noreply.github.com>
2023-11-24 14:30:29 +0900

    発展課題を追加
    ・ボスクラスを作成
    ・魔法攻撃を行うFor文を作成

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public class Boss
+{
+    int mp = 53;
+
+    public void Magic()
+    {
+        if(mp >= 5)
+        {
+            mp = mp - 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
+    }
+}
 
 public class Test : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +38,17 @@ public class Test : MonoBehaviour
         {
             Debug.Log(array[i]);
         }
+
+        //発展課題
+        Boss bossLesson = new Boss();
+
+        for(int i =0;i<=15;i++)
+        {
+            int turnCount = i + 1;
+            Debug.Log("ターン" + turnCount);
+            bossLesson.Magic();
+        }
+
     }
 
     // Update is called once per frame
